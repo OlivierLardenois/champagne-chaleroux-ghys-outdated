3a6ec8a5eda3cc620331794dd53b32d454924eff
OlivierLardenois <olivier@gymlib.com>
2019-04-03 22:57:47 +0200

    💄 - Create layout component

---
+++ src/components/footer.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const StyledFooter = styled.footer`
+    border: solid;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: 70px;
+    width: 100%;
+`;
+
+const FooterCard = styled.div`
+    border: solid;
+`;
+
+const Footer = () => (
+    <StyledFooter>
+        <FooterCard>
+            <div>Footer</div>
+        </FooterCard>
+    </StyledFooter>
+);
+
+export default Footer;
--- src/components/header.js
+++ src/components/header.js
@@ -1,42 +1,42 @@
-import { Link } from 'gatsby';
-import PropTypes from 'prop-types';
 import React from 'react';
+import styled from 'styled-components';
 
-const Header = ({ siteTitle }) => (
-    <header
-        style={{
-            background: `rebeccapurple`,
-            marginBottom: `1.45rem`,
-        }}
-    >
-        <div
-            style={{
-                margin: `0 auto`,
-                maxWidth: 960,
-                padding: `1.45rem 1.0875rem`,
-            }}
-        >
-            <h1 style={{ margin: 0 }}>
-                <Link
-                    to="/"
-                    style={{
-                        color: `white`,
-                        textDecoration: `none`,
-                    }}
-                >
-                    {siteTitle}
-                </Link>
-            </h1>
-        </div>
-    </header>
+const HeaderItem = styled.div`
+    margin: 0 20px;
+`;
+
+const StyledHeader = styled.header`
+    display: flex;
+    justify-content: space-between;
+`;
+
+const HeaderItems = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+    position: fixed;
+    height: 60px;
+    width: 100%;
+    border: solid;
+`;
+
+const LogoWapper = styled.div``;
+
+const Header = () => (
+    <StyledHeader>
+        <LogoWapper />
+        <HeaderItems>
+            <HeaderItem>
+                <a>Présentation</a>
+            </HeaderItem>
+            <HeaderItem>
+                <a>Accès</a>
+            </HeaderItem>
+            <HeaderItem>
+                <a>Contact</a>
+            </HeaderItem>
+        </HeaderItems>
+    </StyledHeader>
 );
-
-Header.propTypes = {
-    siteTitle: PropTypes.string,
-};
-
-Header.defaultProps = {
-    siteTitle: ``,
-};
 
 export default Header;
