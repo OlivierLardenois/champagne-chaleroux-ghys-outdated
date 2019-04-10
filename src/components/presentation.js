286197edf98a3a6c8285cb4b2f8d14cbbb60cdcd
OlivierLardenois <olivier@gymlib.com>
2019-04-10 13:37:02 +0200

    ✨ - Add presentation

---
+++ src/components/presentation.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import styled from 'styled-components';
+import { injectIntl } from 'gatsby-plugin-intl';
+
+const Presentation = ({ intl }) => (
+    <p>
+        Sea oporteat hendrerit scripserit ea. His id diam dolor ridens. Ullum veniam ei
+        ius. Iuvaret gubergren consequuntur at nec. Eu qui dicam meliore conclusionemque,
+        has vidisse feugiat offendit id, duo prima pericula cu. <br /> <br /> Mea nostrud
+        vivendum vulputate in. Vix aperiam similique ad. Cu mel omnis tritani, in nam
+        blandit indoctum, utinam viderer atomorum ex duo. An qui probo posidonium, his et
+        ferri molestie. Has an gubergren torquatos, ex sea velit diceret persequeris.
+    </p>
+);
+
+export default injectIntl(Presentation);
--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import { Link } from 'gatsby';
 import Contact from '../components/contact';
 import Layout from '../components/layout';
 import Image from '../components/image';
+import Presentation from '../components/presentation';
 import SEO from '../components/seo';
 
 const IndexPage = () => (
@@ -11,6 +12,7 @@ const IndexPage = () => (
         <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
         <Image />
         <Link to="/page-2/">Go to page 2</Link>
+        <Presentation />
         <Contact />
     </Layout>
 );
