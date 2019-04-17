16bb305852f1f296351cef9cb3dab17ac04599df
OlivierLardenois <olivier@gymlib.com>
2019-04-17 13:53:32 +0200

    🚧 - Add footer items

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,26 +1,93 @@
 import React from 'react';
 import styled from 'styled-components';
+import { injectIntl } from 'gatsby-plugin-intl';
+import { Link } from 'react-scroll';
+
+import Language from './language';
 
 const StyledFooter = styled.footer`
-    border: solid;
-    box-sizing: border-box;
     display: flex;
-    align-items: center;
+    font-family: 'Open sans';
     justify-content: center;
-    height: 70px;
-    width: 100%;
+    padding: 20px 0;
 `;
 
 const FooterCard = styled.div`
-    border: solid;
+    background: white;
+    box-sizing: border-box;
+    box-shadow: 0px 3px 8px 1px #e5e5e5;
+    display: flex;
+    justify-content: space-between;
+    max-width: 800px;
+    padding: 30px 70px;
+    width: 100%;
 `;
 
-const Footer = () => (
+const FooterLink = styled.div`
+    ul {
+        padding: 0;
+    }
+
+    h3 {
+        font-size: 1.1em;
+        margin-bottom: 20px;
+    }
+
+    li {
+        display: block;
+        margin: 5px 0;
+
+        a {
+            cursor: pointer;
+            font-size: 0.8em;
+            text-decoration: none;
+            &:hover {
+                color: blue;
+            }
+        }
+    }
+`;
+
+const FooterActionsWrapper = styled.div`
+    padding: 10px 0;
+    width: 250px;
+`;
+
+const Scroller = ({ children, to }) => (
+    <Link to={to} smooth="easeOutQuart" duration={1000}>
+        {children}
+    </Link>
+);
+
+const Footer = ({ intl }) => (
     <StyledFooter>
         <FooterCard>
-            <div>Footer</div>
+            <FooterLink>
+                <h3>{intl.formatMessage({ id: 'sitemap' })}</h3>
+                <ul>
+                    <li>
+                        <Scroller to="presentation">
+                            {intl.formatMessage({ id: 'presentation' })}
+                        </Scroller>
+                    </li>
+                    <li>
+                        <Scroller to="access">
+                            {intl.formatMessage({ id: 'access' })}
+                        </Scroller>
+                    </li>
+                    <li>
+                        <Scroller to="contact">
+                            {intl.formatMessage({ id: 'contact' })}
+                        </Scroller>
+                    </li>
+                </ul>
+            </FooterLink>
+            <FooterActionsWrapper>
+                <Language />
+                Add social media
+            </FooterActionsWrapper>
         </FooterCard>
     </StyledFooter>
 );
 
-export default Footer;
+export default injectIntl(Footer);
